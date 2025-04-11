3885e2700b55ff88050ff4a87ec74d7a2dce65f4
ryan-way <ryan.keith.way@gmail.com>
2025-04-11 12:27:43 -0700

    Adding script which does single updates

---
+++ packages/script/src/index.ts
@@ -0,0 +1,116 @@
+import { AttributeAction, DeleteItemCommand, DeleteItemCommandInput, DynamoDBClient, PutItemCommand, ScanCommand, ScanCommandInput, UpdateItemCommand, UpdateItemCommandInput } from "@aws-sdk/client-dynamodb";
+import { Profiler } from "./profiler";
+
+const TABLE_ARN = "arn:aws:dynamodb:us-west-1:196728492750:table/DynamoStack-TestTable5769773A-WBGE6PUF9M82";
+const TEST_SIZE = 100;
+
+async function insertItems(client: DynamoDBClient) {
+    console.log("Inserting items...");
+    for (let i = 0; i < TEST_SIZE; i++) {
+        const input = {
+            TableName: TABLE_ARN,
+            Item: {
+                pk: {
+                    S: `primary_key_${i}`,
+                },
+                num: {
+                    N: `${i}`
+                }
+            }
+        }
+
+        const command = new PutItemCommand(input);
+        await profiler.profileAsync('insert', async () => await client.send(command));
+    }
+}
+
+async function updateItems(client: DynamoDBClient) {
+    console.log("Updating items...");
+    let scanInput: ScanCommandInput = {
+        TableName: TABLE_ARN,
+        Limit: 1,
+        ExclusiveStartKey: undefined,
+    }
+
+    do {
+        const scanCommand = new ScanCommand(scanInput);
+        const scanOutput = await profiler.profileAsync('scan', async () => await client.send(scanCommand));
+
+        scanOutput.Items = scanOutput.Items ?? [];
+        for (const item of scanOutput.Items) {
+            const updateInput: UpdateItemCommandInput = {
+                TableName: TABLE_ARN,
+                Key: {
+                    pk: item.pk
+                },
+                AttributeUpdates: {
+                    "str": {
+                        Value: item.pk,
+                        Action: AttributeAction.PUT
+                    },
+                }
+            }
+
+            const updateCommand = new UpdateItemCommand(updateInput);
+            await profiler.profileAsync('update', async () => await client.send(updateCommand));
+        }
+
+        scanInput.ExclusiveStartKey = scanOutput.LastEvaluatedKey;
+    } while (scanInput.ExclusiveStartKey);
+}
+
+async function deleteItems(client: DynamoDBClient) {
+    console.log("Deleting items...");
+    let scanInput: ScanCommandInput = {
+        TableName: TABLE_ARN,
+        Limit: 1,
+        ExclusiveStartKey: undefined,
+    }
+
+    do {
+        const scanCommand = new ScanCommand(scanInput);
+        const scanOutput = await profiler.profileAsync('scan', async () => await client.send(scanCommand));
+
+        scanOutput.Items = scanOutput.Items ?? [];
+        for (const item of scanOutput.Items) {
+            const deleteInput: DeleteItemCommandInput = {
+                TableName: TABLE_ARN,
+                Key: {
+                    pk: item.pk
+                },
+            }
+
+            const deleteCommand = new DeleteItemCommand(deleteInput);
+            await profiler.profileAsync('delete', async () => await client.send(deleteCommand));
+        }
+
+        scanInput.ExclusiveStartKey = scanOutput.LastEvaluatedKey;
+    } while (scanInput.ExclusiveStartKey);
+}
+
+const client = new DynamoDBClient({ profile: 'dynamo-migration'});
+const profiler = new Profiler();
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.log("Usage: npm run start [insert|update|delete]");
+    }
+
+    switch (process.argv[2]) {
+        case "insert":
+            await insertItems(client);
+            break;
+        case "update":
+            await updateItems(client);
+            break;
+        case "delete":
+            await deleteItems(client);
+            break;
+        default:
+            throw new Error(`Unsupported command ${process.argv[2]}`);
+    }
+
+    profiler.report((time) => { return { "time per item": Math.round((time/TEST_SIZE)*100)/100 }});
+    console.log("Done!");
+}
+main();
---
+++ packages/script/src/profiler.ts
@@ -0,0 +1,65 @@
+// async function delay(ms: number): Promise<void> {
+//     return new Promise( resolve => setTimeout(resolve, ms) );
+// }
+//
+// async function profile() {
+//     const profiler = new Profiler();
+
+//     await profiler.profileAsync("test", () => delay(1000));
+//     await profiler.profileAsync("test2", () => delay(500));
+//     await profiler.profileAsync("test3", () => delay(100));
+
+//     profiler.report();
+// }
+// ┌─────────┬─────────┬───────────┬─────────┐
+// │ (index) │ profile │ time (ms) │ percent │
+// ├─────────┼─────────┼───────────┼─────────┤
+// │ 0       │ 'test'  │ 1000.64   │ 0.62    │
+// │ 1       │ 'test2' │ 500.86    │ 0.31    │
+// │ 2       │ 'test3' │ 100.4     │ 0.06    │
+// └─────────┴─────────┴───────────┴─────────┘
+
+export class Profiler {
+    private map: Map<string, number> = new Map<string, number>();
+    constructor() {}
+
+    profile<T>(key: string, fn: () => T): T {
+        const start = performance.now();
+        const result = fn();
+        const end = performance.now();
+
+        const prev = this.map.get(key);
+        this.map.set(key, (end - start) + (prev ?? 0));
+
+        return result;
+    }
+
+    async profileAsync<T>(key: string, fn: () => Promise<T>): Promise<T> {
+        const start = performance.now();
+        const result = await fn();
+        const end = performance.now();
+
+        const prev = this.map.get(key);
+        this.map.set(key, (end - start) + (prev ?? 0));
+
+        return result;
+    }
+
+    report(additional: (time: number) => any = () => {}) {
+
+        const report = [];
+        let sum = 0;
+        for(const entry of this.map) {
+            sum += entry[1];
+        }
+        for(const entry of this.map) {
+            report.push({
+                profile: entry[0],
+                "time (ms)": Math.round(entry[1]*100)/100,
+                percent: Math.round((entry[1]/sum)*100)/100,
+                ...additional(Math.round(entry[1]*100)/100)
+            })
+        }
+        console.table(report);
+    }
+}
